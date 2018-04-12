6ead63886d73b4e5005f40c18a797523891d74ef
cwang101 <625250454@qq.com>
2018-04-12 14:56:06 +0800

    section3 PracticeA complete!

--- src/main/java/cn/school/thoughtworks/section3/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeA.java
@@ -1,12 +1,23 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeA {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        Map<String, Integer> result = new HashMap<>();
+        List<String> target=object.get("value");
+        for(Map.Entry<String,Integer> entryA:collectionA.entrySet()){
+            String key=entryA.getKey();
+            int value=entryA.getValue();
+            if(target.contains(key)){
+                result.put(key,value-1);
+            }else {
+                result.put(key,value);
+            }
+        }
+        return result;
     }
 }

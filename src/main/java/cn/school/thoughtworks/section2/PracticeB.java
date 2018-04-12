45223c58f95a672345921c767f502ac6f42d25a3
cwang101 <625250454@qq.com>
2018-04-12 14:15:35 +0800

    section2 PracticeB complete!

--- src/main/java/cn/school/thoughtworks/section2/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeB.java
@@ -1,12 +1,28 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeB {
     Map<String, Integer> countSameElements(List<String> collection1) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        Map<String, Integer> result = new HashMap<>();
+        for(String element:collection1){
+            String[] splits=element.split("-",-1);
+            int increase;
+            if(splits.length<=1){
+                increase=1;
+            }else {
+                increase=Integer.valueOf(splits[1]);
+            }
+            Integer num=result.get(splits[0]);
+            if (num==null){
+                result.put(splits[0],increase);
+            }else {
+                result.put(splits[0],num+increase);
+            }
+        }
+        return result;
     }
 }

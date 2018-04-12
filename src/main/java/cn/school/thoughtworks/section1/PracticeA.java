d547c8e9e3f372c4cbda6b605e07009a9dca263f
cwang101 <625250454@qq.com>
2018-04-12 13:58:55 +0800

    section1 completed!

--- src/main/java/cn/school/thoughtworks/section1/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeA.java
@@ -1,10 +1,15 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PracticeA {
     List<String> collectSameElements(List<String> collection1, List<String> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        List<String> copy1=new ArrayList<>(collection1);
+        List<String> copy2=new ArrayList<>(collection1);
+        copy1.removeAll(collection2);
+        copy2.removeAll(copy1);
+        return copy2;
     }
 }
--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,16 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        List<String> result=new ArrayList<>();
+        PracticeA practiceA=new PracticeA();
+        for(List<String> childList:collection2){
+            result.addAll(practiceA.collectSameElements(collection1,childList));
+        }
+        return result;
     }
 }
